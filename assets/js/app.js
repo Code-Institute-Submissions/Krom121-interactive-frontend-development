78ffc8ae7bf49459a5444e234fe1dce78fbd1128
stephen <stephenmacgregor9@gmail.com>
2020-01-29 07:14:16 +0000

    decided on different charts with apis

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,27 +19,3 @@ treeData("assets/data/treeData.csv", 'id', 'parent').then(result => {
 })
 
 
-// import data for interactive bar & pie charts
-
-let justStates = null,
-rootData = null;
-
-povertyData('assets/data/statePoverty.json').then(res => {
-  // get just the states for the buttons
-	  justStates = res.map(d =>{
-		  return {State: d.State, checked: d.selected}
-	  })
-
-	//return states data for buttons, res for chart Data
-	  return {justStates, res}
-    }).then(({justStates, res}) => {
-
-	//build the buttons
-	buildButtons(justStates, 'buttonWrapper');
-
-	rootData = res;
-
-	//add button events
-	let radios = d3.selectAll('.buttonG')
-	radios.on('click', updateSelectedState)
-})

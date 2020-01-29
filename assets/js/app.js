1bdeabe10a73c10a592a2dc8e0c92a95353a4638
stephen <stephenmacgregor9@gmail.com>
2020-01-29 06:04:47 +0000

    bug fixes type errors

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,22 +24,22 @@ treeData("assets/data/treeData.csv", 'id', 'parent').then(result => {
 let justStates = null,
 rootData = null;
 
-povertyData("assets/data/statePoverty.json").then(res => {
-    // get just the states for the buttons
-    let justStates = res.map(d => {
-      return {State: d.State, checked: d.selected}
-    })
-    // return states data for buttons, result for chart data
-    return {justStates, res}
+povertyData('assets/data/statePoverty.json').then(res => {
+  // get just the states for the buttons
+	  justStates = res.map(d =>{
+		  return {State: d.State, checked: d.selected}
+	  })
 
-}).then(({justStates, res}) => {
-    //build the buttons
-    buildButtons(justStates, 'buttonWrapper');
+	//return states data for buttons, res for chart Data
+	  return {justStates, res}
+    }).then(({justStates, res}) => {
 
-    rootData = res;
+	//build the buttons
+	buildButtons(justStates, 'buttonWrapper');
 
-    //add button events
-    let radios = d3.selectAll('.buttonG')
-    radios.on('click', updateSelectedState)
-    // get & show the currently selected data
+	rootData = res;
+
+	//add button events
+	let radios = d3.selectAll('.buttonG')
+	radios.on('click', updateSelectedState)
 })
--- assets/js/lib.js
+++ assets/js/lib.js
@@ -14,53 +14,55 @@ function appendToParent(parent, type, className, transformation){
 }
 
 function buildButtons(srcData, parentID){
-    let parent = d3.select(`#${parentID}`)
-    let dataJoin = parent
-        .selectAll('g')
-        .data(srcData);
-        dataJoin.join(enterRadio, updateRadio)
+	let parent = d3.select(`#${parentID}`)
+	let dataJoin = parent
+		.selectAll('g')
+		.data(srcData);
+		dataJoin.join(enterRadio, updateRadio)
 }
 
-function enterRadio(enterSelection) {
-    let groups = enterSelection.append('g')
-    .attr('class', 'buttonG')
+function enterRadio(enterSelection){
+	let groups = enterSelection.append('g')
+	.attr('class', 'buttonG');
 
-    groups.append('input')
-    .attrs({
-        'type': 'radio',
-        'value': d => d.State,
-        'class': 'stateRadio',
-        'checked': d => (d.checked == true) ? true : null,
-        'name': 'selectedState'
-    })
-
-    groups.append('text')
-        .text(d => d.State)
+	groups.append('input')
+	.attrs({
+		'type': 'radio',
+		'value': d => d.State,
+		'class': 'stateRadio',
+		'checked': d => (d.checked == true) ? true : null,
+		'name': 'selectedState'
+	})
+	
+	groups.append('text')
+		.text(d => d.State)
 }
 
-function updateRadio(updateSelection) {
-    updateSelection.select('.stateRadio')
-        .call(u => u.transition().duration(0)
-        .attr('checked', d => {
-            return (d.checked == ture) ? true : null
-        }))
-    }
+function updateRadio(updateSelection){	
+	updateSelection.select('.stateRadio')
+		.call(u => u.transition().duration(0)
+		.attr('checked', d => {
+			return (d.checked == true) ? true : null
+		}))
+}
 
+function updateSelectedState(e){
 
-function updateSelectedState(e) {
-    // update the selected state status in our data
-    justStates = justStates.map(d => {
-        d.checked = false;
+	// update the selected state status in our data
+	justStates = justStates.map(d => {
+		d.checked = false;
 
-        if(d.State == e.State) {
-
-        }
-        return d
-    })
-
-    // update buttons
-    buildButtons(justStates, 'buttonWrapper')
-
-    // updated selected data
-    let selectedData = rootData.filter(d => d.State == justStates.filter())
+		if(d.State == e.State){
+			d.checked = true;
+		}
+		return d
+	})
+	
+	//update buttons
+	buildButtons(justStates, 'buttonWrapper')
+	
+		//update selected data
+	let selectedData = rootData.filter(d => d.State == justStates.filter(s => s.checked == true)[0].State)[0]
+	console.log('selectedData')
+	console.log(selectedData)
 }
